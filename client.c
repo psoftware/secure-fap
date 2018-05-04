60aac43821cddfbb9d856d8a392a421df3116545
Vincenzo <vincenzo.consales95@gmail.com>
2018-05-04 19:14:19 +0200

    Aggiunti controlli errori. Rimosse variabili non usate

--- client.c
+++ client.c
@@ -140,10 +140,8 @@ void encrypt_antonio(int cl_sock, const char *filename)
 int main(int argc, char **argv) 
 {
 	int sd;
-	char filename[128];
 	unsigned char *buffer_file = NULL;
 	unsigned int file_len = 0;
-	my_buffer my_buff = {NULL, 0};
 
 	uint16_t server_port;
 
@@ -153,10 +151,9 @@ int main(int argc, char **argv)
 	unsigned char *iv;
 
 	unsigned char* encrypted_keys[1];
-	unsigned int encrypted_keys_len[1];
-
+	int encrypted_keys_len[1];
 	EVP_PKEY* pubkeys[1];
-	FILE* file_pubkey;
+	int evp_res;
 
 	if( argc < 3 ){
 		perror("use: ./client filename server_ip port");
@@ -169,9 +166,11 @@ int main(int argc, char **argv)
 	// leggo l contenuto del file da inviare
 	file_len = readcontent(argv[1],&buffer_file);
 
-	file_pubkey = fopen("keys/rsa_server_pubkey.pem", "r");
-	pubkeys[0] = PEM_read_PUBKEY(file_pubkey, NULL, NULL, NULL);
-	fclose(file_pubkey);
+	if( !read_pub_key("keys/rsa_server_pubkey.pem",pubkeys) ){
+		printf("Cannot read public key file\n");
+		return -1;
+	}
+
 
 	encrypted_keys_len[0] = EVP_PKEY_size(pubkeys[0]);
 	encrypted_keys[0] = malloc(encrypted_keys_len[0]);
@@ -179,20 +178,35 @@ int main(int argc, char **argv)
 
 	ctx = malloc(sizeof(EVP_CIPHER_CTX));
 	iv = malloc(EVP_CIPHER_iv_length(EVP_aes_128_cbc())); 
-	EVP_SealInit(ctx, EVP_aes_128_cbc(), encrypted_keys, encrypted_keys_len, iv, pubkeys, 1);
-	EVP_SealUpdate(ctx, ciphertext, &outlen, (unsigned char*)buffer_file, file_len);
+	if( iv == NULL ){
+		printf("Cannot allocate iv \n");
+		return -1;
+	}
+	evp_res = EVP_SealInit(ctx, EVP_aes_128_cbc(), encrypted_keys, encrypted_keys_len, iv, pubkeys, 1);
+	if(evp_res == 0)
+		printf("EVP_SealInit Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+
+	evp_res = EVP_SealUpdate(ctx, ciphertext, &outlen, (unsigned char*)buffer_file, file_len);
+	if(evp_res == 0)
+		printf("EVP_SealUpdate Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+
 	cipherlen = outlen;
-	EVP_SealFinal(ctx, ciphertext+cipherlen, &outlen);
+	evp_res = EVP_SealFinal(ctx, ciphertext+cipherlen, &outlen);
+	if(evp_res == 0)
+		printf("EVP_SealFinal Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+
 	cipherlen += outlen;
 
 	//printf("encrypted_keys_len:%d\n",encrypted_keys_len[0]);
-	send_data(sd,(char*)encrypted_keys[0], encrypted_keys_len[0]);
+	send_data(sd,encrypted_keys[0], encrypted_keys_len[0]);
 	//printf("encrypted_keys:%20s\n",encrypted_keys[0]);
 	printf("iv_len:%d\n",EVP_CIPHER_iv_length(EVP_aes_128_cbc()));
 	send_data(sd,iv,EVP_CIPHER_iv_length(EVP_aes_128_cbc()));
 	//printf("cipherlen:%d\n\n",cipherlen);
  	send_data(sd,ciphertext,cipherlen);
 
+ 	EVP_CIPHER_CTX_cleanup(ctx);
+	free(ctx);
 	close(sd);
 
 	return 0;
