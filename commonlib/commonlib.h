ea5e006258b2057a07a80c1e6aacecde5cafc4f9
Antonio le Caldare <antonio.lecaldare@gmail.com>
2018-05-04 23:11:45 +0200

    commonlib: added compute_hash function

--- commonlib/commonlib.h
+++ commonlib/commonlib.h
@@ -12,6 +12,14 @@ typedef enum { false, true } bool;
 #include <fcntl.h>
 #include <unistd.h>
 
+#define HMAC_LENGTH 32
+
+/* ##### OpenSSL Help Functions ##### */
+unsigned int hmac_compute(
+	unsigned char *inputdata[], unsigned int inputdata_length[], unsigned int inputdata_count,
+	unsigned char *key, unsigned int key_length,
+	unsigned char *hash_output);
+
 // Utils
 void print_hex(unsigned char* buff, unsigned int size);
 
