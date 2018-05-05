69f114eab4ea97f6a88622070861b88653addf52
Antonio le Caldare <antonio.lecaldare@gmail.com>
2018-05-05 06:15:25 +0200

    added DecryptSession class

--- commonlib/commonlib.cpp
+++ commonlib/commonlib.cpp
@@ -133,6 +133,67 @@ EncryptSession::~EncryptSession()
 	delete encrypted_keys[0];
 }
 
+
+
+bool DecryptSession::read_prv_key(const char *filename)
+{
+	FILE* file = fopen(filename, "r");
+
+	if(file == NULL)
+		return false;
+
+	prvkey = PEM_read_PrivateKey(file, NULL, NULL, NULL);
+	if(prvkey == NULL)
+		return false;
+
+	fclose(file);
+	return true;
+}
+
+DecryptSession::DecryptSession(const char* prvkey_path, unsigned char *session_key, unsigned int session_key_len, unsigned char *iv) {
+	//"keys/rsa_server_privkey.pem"
+	if(!read_prv_key(prvkey_path)) {
+		printf("Key read error...\n");
+		return;
+	}
+
+	ctx = new EVP_CIPHER_CTX;
+	EVP_CIPHER_CTX_init(ctx);
+
+	int evp_res = EVP_OpenInit(ctx, EVP_aes_128_cbc(), session_key, session_key_len, iv, prvkey);
+	if(evp_res == 0)
+		printf("EVP_OpenInit Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+}
+
+unsigned int DecryptSession::decrypt(unsigned char *partial_ciphertext, unsigned int partial_cipherlen, unsigned char **partial_plaintext)
+{
+	*partial_plaintext = new unsigned char[partial_cipherlen];
+
+	int outlen;
+	int evp_res = EVP_OpenUpdate(ctx, *partial_plaintext, &outlen, partial_ciphertext, partial_cipherlen);
+	if(evp_res == 0)
+		printf("EVP_OpenUpdate Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+
+	return (unsigned int)outlen;
+}
+
+unsigned int DecryptSession::decrypt_end(unsigned char *latest_partial_plaintext, unsigned int latest_partial_plainlen)
+{
+	int outlen;
+	int evp_res = EVP_OpenFinal(ctx, latest_partial_plaintext + latest_partial_plainlen, &outlen);
+	if(evp_res == 0)
+		printf("EVP_OpenFinal Error: %s\n", ERR_error_string(ERR_get_error(), NULL));
+
+	return (unsigned int)outlen;
+}
+
+DecryptSession::~DecryptSession()
+{
+	EVP_CIPHER_CTX_cleanup(ctx);
+	free(ctx);
+
+	delete prvkey;
+}
 /*
 	print_hex((void*)(&encrypted_keys[0][0]), encrypted_keys_len[0]);
 	print_hex(iv, EVP_CIPHER_iv_length(EVP_aes_128_cbc()));
--- commonlib/commonlib.h
+++ commonlib/commonlib.h
@@ -49,6 +49,23 @@ public:
 	~EncryptSession();
 };
 
+class DecryptSession {
+private:
+	EVP_PKEY* prvkey;
+
+	EVP_CIPHER_CTX* ctx;
+
+	bool read_prv_key(const char *filename);
+
+public:
+	DecryptSession(const char* prvkey_path, unsigned char *encrypted_key, unsigned int encrypted_key_len, unsigned char *iv);
+
+	unsigned int decrypt(unsigned char *partial_ciphertext, unsigned int partial_cipherlen, unsigned char **partial_plaintext);
+	unsigned int decrypt_end(unsigned char *latest_partial_plaintext, unsigned int latest_partial_plainlen);
+
+	~DecryptSession();
+};
+
 // Utils
 void print_hex(unsigned char* buff, unsigned int size);
 
